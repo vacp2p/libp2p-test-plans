7ff157814ed349f917644ff9ae4da7bcff5e3b5d
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2024-08-22 16:15:20 +0200

    wip

--- transport-interop/dockerBuildWrapper.sh
+++ transport-interop/dockerBuildWrapper.sh
@@ -3,10 +3,14 @@
 CACHING_OPTIONS=""
 # If in CI and we have a defined cache bucket, use caching
 if [[ -n "${CI}" ]] && [[ -n "${AWS_BUCKET}" ]]; then
-    CACHING_OPTIONS="\
-        --cache-to   type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME \
-        --cache-from type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME"
+    # CACHING_OPTIONS="\
+    #     --cache-to   type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME \
+    #     --cache-from type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME"
 fi
+
+CACHING_OPTIONS="\
+        --cache-to   type=gha,mode=max \
+        --cache-from type=gha,mode=max"
 
 docker buildx build \
     --load \
